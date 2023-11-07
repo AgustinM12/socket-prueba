998dac3d7d4982fb2dc4e7cad6a2b1ac4e0006d6
AgustinM12 <agustinmazza12345@gmail.com>
2023-11-06 23:49:28 -0300

    se completaron las funciones

--- app.js
+++ app.js
@@ -20,11 +20,36 @@ const io = new Server(server, {
 })
 
 const messageList = []
+const typingUsers = new Set();
 
 //configuracion
 io.on('connection', (socket) => {
     //al conectarse
     console.log(socket.id, `New socket connected`);
+
+
+    // Manejar evento de escritura
+    socket.on('typing', (userName) => {
+        socket.broadcast.emit("typing", userName)
+
+        // Agrega el usuario a la lista de usuarios escribiendo
+        // typingUsers.add(userName);
+
+        // Emite la lista de usuarios escribiendo a todos los clientes
+        // io.emit('typing', Array.from(typingUsers));
+
+    });
+
+
+    // Manejar evento dejar de escritura
+    socket.on('stopTyping', (userName) => {
+        // Elimina el usuario de la lista de usuarios escribiendo
+        typingUsers.delete(userName);
+
+        // Emite la lista actualizada de usuarios escribiendo a todos los clientes
+        io.emit('typing', Array.from(typingUsers));
+    });
+
 
     //al desconectar
     socket.on("disconnect", () => {
